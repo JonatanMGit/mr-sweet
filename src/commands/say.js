ff00018fb4983a9e035b3a6c7ffc625910f1f379
JonatanMGit <jonatan@miarecki.eu>
2023-01-03 13:29:55 +0100

    Make the say command not show the user

--- src/commands/say.js
+++ src/commands/say.js
@@ -10,6 +10,9 @@ module.exports = {
                 .setRequired(true)),
     async execute(interaction) {
         const input = interaction.options.getString('input');
-        await interaction.reply(input);
+        // reply with the input but dont show who said it
+        await interaction.channel.send(input, { allowedMentions: {} });
+        // finish the interaction
+        await interaction.reply({ content: 'Sent!', ephemeral: true });
     },
 };
