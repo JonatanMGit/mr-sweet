0db8520bb1a7fa51eda9c1fd4ec12ec3dbdd42df
JonatanMGit <jonatan@miarecki.eu>
2023-01-20 16:56:28 +0100

    improve leave logging NOTE: somehow improve getting the channel if it isn't found

--- src/events/leave.ts
+++ src/events/leave.ts
@@ -6,9 +6,19 @@ module.exports = {
     name: Events.GuildMemberRemove,
     async execute(event) {
         console.log(`User ${event.user.tag} left the guild.`);
-        const channel = event.guild.channels.cache.get('1051064356026187846');
+        let channel = event.guild.channels.cache.get('1051064356026187846');
+        // if channel not if cache, fetch it
+        if (!channel) {
+            const fetchedChannel = await event.guild.channels.fetch('1051064356026187846');
+            if (!fetchedChannel) {
+                console.log('Channel not found');
+                return;
+            }
+            channel = fetchedChannel;
+        }
+        // send the message
 
-        const message = `User ${event.user.tag} left the guild with the following data:
+        const message = `User ${event.user.tag} left the guild (${event.guild.name})) with the following data:
 \`\`\`json\n${JSON.stringify(event.user, null, 4)}\`\`\`Time: <t:${Math.floor(Date.now() / 1000)}:R>`;
 
         await channel.send(message);
