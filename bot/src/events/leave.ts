6d5a32f814c1dc65cf938b955d20715310193732
JonatanMGit <jonatan@miarecki.eu>
2023-01-31 15:14:08 +0100

    Fix leave logging

--- bot/src/events/leave.ts
+++ bot/src/events/leave.ts
@@ -1,15 +1,17 @@
-import { Events } from 'discord.js';
+import { Events, TextChannel } from 'discord.js';
+import client from '../index';
 
 module.exports = {
     // send a message to the channel 1051064356026187846 when a user leaves
     // the time is an unix timestamp so like <t:1620000000:R>
     name: Events.GuildMemberRemove,
     async execute(event) {
-        console.log(`User ${event.user.tag} left the guild.`);
-        let channel = event.guild.channels.cache.get('1051064356026187846');
+        console.log(`User ${event.user.tag} left the guild (${event.guild.name}) with the following data:`);
+        const guild = client.guilds.cache.get('813852446069751838');
+        let channel = guild.channels.cache.get('1051064356026187846') as TextChannel;
         // if channel not if cache, fetch it
         if (!channel) {
-            const fetchedChannel = await event.guild.channels.fetch('1051064356026187846');
+            const fetchedChannel = await guild.channels.fetch('1051064356026187846') as TextChannel;
             if (!fetchedChannel) {
                 console.log('Channel not found');
                 return;
@@ -18,7 +20,7 @@ module.exports = {
         }
         // send the message
 
-        const message = `User ${event.user.tag} left the guild (${event.guild.name})) with the following data:
+        const message = `User ${event.user.tag} left the guild (${event.guild.name}) with the following data:
 \`\`\`json\n${JSON.stringify(event.user, null, 4)}\`\`\`Time: <t:${Math.floor(Date.now() / 1000)}:R>`;
 
         await channel.send(message);
