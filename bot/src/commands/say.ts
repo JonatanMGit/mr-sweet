08646866fccb7c3f645909cd19a8f438c9622e2d
JonatanMGit <jonatan@miarecki.eu>
2023-02-03 16:57:41 +0100

    Add file upload to say

--- bot/src/commands/say.ts
+++ bot/src/commands/say.ts
@@ -7,11 +7,30 @@ module.exports = {
         .addStringOption(option =>
             option.setName('input')
                 .setDescription('The input to echo back')
-                .setRequired(true)),
+                .setRequired(false))
+        .addAttachmentOption(option =>
+            option.setName('attachment')
+                .setDescription('The attachment to send')
+                .setRequired(false)),
     async execute(interaction) {
         const input = interaction.options.getString('input');
-        // reply with the input but dont show who said it
-        await interaction.channel.send(input, { allowedMentions: {} });
+        const attachment = interaction.options.getAttachment('attachment');
+        let message = { allowedMentions: {} };
+        /*
+        if (attachment && interaction.author.id !== '1') {
+            await interaction.reply({ content: 'This feature is reserved for Mr Sweet pro users only!', ephemeral: true });
+            return;
+        }
+        */
+        // add each option to the message inidividually and allow one to be missing
+        if (input) {
+            message['content'] = input;
+        }
+        if (attachment) {
+            message['files'] = [attachment];
+        }
+        await interaction.channel.send(message);
+
         // finish the interaction
         await interaction.reply({ content: 'Sent!', ephemeral: true });
     },
