2bccb9257565058b5c1b94187dc3a39f919ca3cf
JonatanMGit <jonatan@miarecki.eu>
2023-04-14 18:33:31 +0200

    feat: Prompt message streaming

--- bot/src/commands/prompt.ts
+++ bot/src/commands/prompt.ts
@@ -3,6 +3,8 @@ import { openai, gpt4Model, gpt3Model, defaultSystemPrompt } from '../ai';
 import { ChatCompletionRequestMessage } from 'openai';
 import { RateLimiter } from 'discord.js-rate-limiter';
 let rateLimiter = new RateLimiter(1, 10000);
+import { getResponse } from '../ai';
+import { ChatInputCommandInteraction, CommandInteraction } from 'discord.js';
 
 
 module.exports = {
@@ -12,7 +14,7 @@ module.exports = {
         .setDescription('Ask Mr Sweet anything')
         .addStringOption(option => option.setName('input').setDescription('Your Message').setRequired(true))
         .addStringOption(option => option.setName('model').setDescription('The model to use').setRequired(false).addChoices({ name: 'Mr Sweet v3', value: 'gpt-3' }, { name: 'Mr Sweet v4', value: 'gpt-4' })),
-    async execute(interaction) {
+    async execute(interaction: ChatInputCommandInteraction) {
         let limited = rateLimiter.take(interaction.user.id);
         if (limited) {
             interaction.reply({
@@ -21,7 +23,9 @@ module.exports = {
             });
             return;
         }
-        await interaction.reply("Thinking...")
+
+        interaction.deferReply({ fetchReply: true });
+
         const input = interaction.options.getString('input');
         const selectedModel = interaction.options.getString('model');
 
@@ -32,14 +36,38 @@ module.exports = {
             model = gpt3Model;
         }
 
-        const message = [{ role: "system", content: defaultSystemPrompt }, { role: "user", content: input }] as ChatCompletionRequestMessage[];
+        const response = await getResponse([{ author: interaction.user.id, content: input }], model);
 
-        const response = await openai.createChatCompletion({
-            model: model,
-            messages: message,
-            max_tokens: 500,
+        let data = '';
+
+
+
+        // update the message with the new data every 1 seconds until the stream is finished
+        const interval = setInterval(() => {
+            interaction.editReply(data);
+        }, 1000);
+
+        response.data.on('data', (chunk) => {
+            const lines = chunk.toString().split('\n').filter(line => line.trim() !== '');
+            // console.log(lines);
+            for (const line of lines) {
+                const message = line.replace(/^data: /, '');
+                //console.log(message);
+                if (message === '[DONE]') {
+                    return; // Stream finished
+                }
+                // 'data: {"id":"chatcmpl-74zxn7a2AWCIAhAezt6fULY64RgSi","object":"chat.completion.chunk","created":1681425855,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":" you"},"index":0,"finish_reason":null}]}'
+                const parsed = JSON.parse(message);
+                if (parsed.choices && parsed.choices[0].delta.content !== undefined) {
+                    // fix bug where the data is undefined
+                    data += parsed.choices[0].delta.content;
+                }
+            }
         });
-
-        await interaction.editReply(response.data.choices[0].message);
+        // stop the interval when the stream is finished, but mak
+        response.data.on('end', () => {
+            clearInterval(interval);
+            interaction.editReply(data);
+        });
     },
 };
