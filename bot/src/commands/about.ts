a54cd92bbeb0f9f6258f840bf460a5980d613614
JonatanMGit <jonatan@miarecki.eu>
2023-04-18 00:12:40 +0200

    fix: typo in about command Co-authored-by: atje <donatbojajj@gmail.com>

--- bot/src/commands/about.ts
+++ bot/src/commands/about.ts
@@ -7,7 +7,7 @@ module.exports = {
         .setDescription('Information about the bot'),
     async execute(interaction) {
         await interaction.reply({
-            content: 'This bot is currently invite only.\nDon\'t miss out on all the exclusive features of this bot! Only select members have access. Get in on the action by having a member with acces invite you!', ephemeral: true
+            content: 'This bot is currently invite only.\nDon\'t miss out on all the exclusive features of this bot! Only select members have access. Get in on the action by having a member with access invite you!', ephemeral: true
         });
     },
 };
