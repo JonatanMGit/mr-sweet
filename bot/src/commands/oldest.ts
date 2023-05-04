e42f27383e6e32870344b958ec91756c53c335ee
JonatanMGit <jonatan@miarecki.eu>
2023-05-04 11:04:11 +0200

    fix: add command to list the oldest users

---
+++ bot/src/commands/oldest.ts
@@ -0,0 +1,24 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { ChatInputCommandInteraction } from 'discord.js';
+
+module.exports = {
+    global: true,
+    data: new SlashCommandBuilder()
+        .setName('oldest')
+        .setDescription('Gets a list of the oldest users in the server'),
+    async execute(interaction: ChatInputCommandInteraction) {
+
+        await interaction.deferReply({ ephemeral: false });
+
+        const members = await interaction.guild.members.fetch();
+
+        const oldestMembers = members.sort((a, b) => a.user.createdAt.getTime() - b.user.createdAt.getTime()).first(20);
+
+        const oldestMembersString = oldestMembers.map(member => `${member.user.username}#${member.user.discriminator} - <t:${Math.floor(member.user.createdAt.getTime() / 1000)}:R>`).join('\n');
+
+        await interaction.editReply({
+            content: `The oldest members in this server are:\n${oldestMembersString}`,
+            allowedMentions: { parse: [] }
+        });
+    },
+};
