1dcd07c05d8f6eadd261c9bdd5dd3e5a90c73e88
JonatanMGit <jonatan@miarecki.eu>
2023-05-14 00:41:18 +0200

    feat: Add hidden prompts

--- bot/src/commands/prompt.ts
+++ bot/src/commands/prompt.ts
@@ -3,8 +3,7 @@ import { gpt4Model, gpt3Model, count_tokens, Message } from '../ai';
 import { RateLimiter } from 'discord.js-rate-limiter';
 let rateLimiter = new RateLimiter(1, 10000);
 import { getResponse, messagesToChatCompletionRequestMessage } from '../ai';
-import { ChatInputCommandInteraction } from 'discord.js';
-
+import { ChatInputCommandInteraction, Message as DiscordMessage } from 'discord.js';
 
 module.exports = {
     global: true,
@@ -12,7 +11,8 @@ module.exports = {
         .setName('prompt')
         .setDescription('Ask Mr Sweet anything')
         .addStringOption(option => option.setName('input').setDescription('Your Message').setRequired(true))
-        .addStringOption(option => option.setName('model').setDescription('The model to use').setRequired(false).addChoices({ name: 'Mr Sweet v3', value: 'gpt-3' }, { name: 'Mr Sweet v4', value: 'gpt-4' })),
+        .addStringOption(option => option.setName('model').setDescription('The model to use').setRequired(false).addChoices({ name: 'Mr Sweet v3', value: 'gpt-3' }, { name: 'Mr Sweet v4', value: 'gpt-4' })
+        ).addBooleanOption(option => option.setName('hidden').setDescription("Don't show the author of the message").setRequired(false)),
     async execute(interaction: ChatInputCommandInteraction) {
         let limited = rateLimiter.take(interaction.user.id);
         if (limited) {
@@ -23,10 +23,13 @@ module.exports = {
             return;
         }
 
-        interaction.deferReply();
 
         const input = interaction.options.getString('input');
         const selectedModel = interaction.options.getString('model');
+        let hidden = interaction.options.getBoolean('hidden');
+        hidden = !hidden;
+
+        await interaction.deferReply();
 
         let model = '';
         if (selectedModel === 'gpt-3') {
@@ -41,12 +44,26 @@ module.exports = {
 
         let data = '';
 
+
+        let sweetMessage: DiscordMessage;
+
+        if (!hidden) {
+            await interaction.deleteReply();
+            sweetMessage = await interaction.channel.send("Thinking...");
+        }
+
+
+
         // update the message with the new data every 1 seconds until the stream is finished
         const interval = setInterval(() => {
-            interaction.editReply(data);
+            if (hidden) {
+                interaction.editReply(data);
+            } else {
+                sweetMessage.edit(data);
+            }
         }, 1000);
 
-        response.data.on('data', (chunk) => {
+        response.data.on('data', (chunk: Buffer) => {
             const lines = chunk.toString().split('\n').filter(line => line.trim() !== '');
             //console.log(lines);
             for (const line of lines) {
@@ -66,7 +83,11 @@ module.exports = {
         // stop the interval when the stream is finished, but mak
         response.data.on('end', () => {
             clearInterval(interval);
-            interaction.editReply(data);
+            if (hidden) {
+                interaction.editReply(data);
+            } else {
+                sweetMessage.edit(data);
+            }
             // push the response to the message array to also count the tokens for the response
             message.push({ author: '1043905318867980530', content: data });
             console.log(messagesToChatCompletionRequestMessage(message));
