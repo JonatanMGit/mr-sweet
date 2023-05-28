b37e284be72c96cb19b6aae5d3887eafb4130bb0
JonatanMGit <jonatan@miarecki.eu>
2023-05-28 22:56:22 +0200

    fix: Crashing on OpenAI errors

--- bot/src/commands/prompt.ts
+++ bot/src/commands/prompt.ts
@@ -38,60 +38,64 @@ module.exports = {
             model = gpt4Model;
         }
 
-        const message = [{ author: interaction.user.id, content: input }] as Message[];
+        try {
+            const message = [{ author: interaction.user.id, content: input }] as Message[];
 
-        const response = await getResponse(message, model);
+            const response = await getResponse(message, model);
 
-        let data = '';
+            let data = '';
 
 
-        let sweetMessage: DiscordMessage;
+            let sweetMessage: DiscordMessage;
 
-        if (!hidden) {
-            await interaction.deleteReply();
-            sweetMessage = await interaction.channel.send("Thinking...");
+            if (!hidden) {
+                await interaction.deleteReply();
+                sweetMessage = await interaction.channel.send("Thinking...");
+            }
+
+
+
+            // update the message with the new data every 1 seconds until the stream is finished
+            const interval = setInterval(async () => {
+                if (hidden) {
+                    await interaction.editReply(data);
+                } else {
+                    await sweetMessage.edit(data);
+                }
+            }, 1000);
+
+            response.data.on('data', (chunk: Buffer) => {
+                const lines = chunk.toString().split('\n').filter(line => line.trim() !== '');
+                //console.log(lines);
+                for (const line of lines) {
+                    const message = line.replace(/^data: /, '');
+                    //console.log(message);
+                    if (message === '[DONE]') {
+                        return; // Stream finished
+                    }
+                    // 'data: {"id":"chatcmpl-74zxn7a2AWCIAhAezt6fULY64RgSi","object":"chat.completion.chunk","created":1681425855,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":" you"},"index":0,"finish_reason":null}]}'
+                    const parsed = JSON.parse(message);
+                    if (parsed.choices && parsed.choices[0].delta.content !== undefined) {
+                        // fix bug where the data is undefined
+                        data += parsed.choices[0].delta.content;
+                    }
+                }
+            });
+            // stop the interval when the stream is finished, but mak
+            response.data.on('end', async () => {
+                clearInterval(interval);
+                if (hidden) {
+                    await interaction.editReply(data);
+                } else {
+                    await sweetMessage.edit(data);
+                }
+                // push the response to the message array to also count the tokens for the response
+                message.push({ author: '1043905318867980530', content: data });
+                console.log(messagesToChatCompletionRequestMessage(message));
+                count_tokens(messagesToChatCompletionRequestMessage(message), interaction.user.id, model);
+            });
+        } catch (e) {
+            interaction.editReply("Mr Sweet is currently unavailable. Please try again later.");
         }
-
-
-
-        // update the message with the new data every 1 seconds until the stream is finished
-        const interval = setInterval(async () => {
-            if (hidden) {
-                await interaction.editReply(data);
-            } else {
-                await sweetMessage.edit(data);
-            }
-        }, 1000);
-
-        response.data.on('data', (chunk: Buffer) => {
-            const lines = chunk.toString().split('\n').filter(line => line.trim() !== '');
-            //console.log(lines);
-            for (const line of lines) {
-                const message = line.replace(/^data: /, '');
-                //console.log(message);
-                if (message === '[DONE]') {
-                    return; // Stream finished
-                }
-                // 'data: {"id":"chatcmpl-74zxn7a2AWCIAhAezt6fULY64RgSi","object":"chat.completion.chunk","created":1681425855,"model":"gpt-3.5-turbo-0301","choices":[{"delta":{"content":" you"},"index":0,"finish_reason":null}]}'
-                const parsed = JSON.parse(message);
-                if (parsed.choices && parsed.choices[0].delta.content !== undefined) {
-                    // fix bug where the data is undefined
-                    data += parsed.choices[0].delta.content;
-                }
-            }
-        });
-        // stop the interval when the stream is finished, but mak
-        response.data.on('end', async () => {
-            clearInterval(interval);
-            if (hidden) {
-                await interaction.editReply(data);
-            } else {
-                await sweetMessage.edit(data);
-            }
-            // push the response to the message array to also count the tokens for the response
-            message.push({ author: '1043905318867980530', content: data });
-            console.log(messagesToChatCompletionRequestMessage(message));
-            count_tokens(messagesToChatCompletionRequestMessage(message), interaction.user.id, model);
-        });
     },
 };
